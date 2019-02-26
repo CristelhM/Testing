c370829263be480e9293d8be76f398873ced4dbf
d4vidmc <d4vidangelmc@gmail.com>
2019-02-26 19:43:29 -0400

    David Factory method

---
+++ src/main/java/david/AbstractFactory.java
@@ -0,0 +1,5 @@
+package main.java.david;
+
+public abstract class AbstractFactory {
+	   abstract Shape getShape(String shapeType) ;
+	}
---
+++ src/main/java/david/Shape.java
@@ -0,0 +1,5 @@
+package main.java.david;
+
+public interface Shape {
+	   void draw();
+	}
